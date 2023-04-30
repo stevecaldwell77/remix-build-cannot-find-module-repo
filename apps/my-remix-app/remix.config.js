774e4fc91544bc5384cbb333efa6c05e566195fd
Stephen Caldwell <scaldwell@gmail.com>
2023-04-30 11:35:27 -0700

    see if setting serverDependenciesToBundle to "all" fixes warning

--- apps/my-remix-app/remix.config.js
+++ apps/my-remix-app/remix.config.js
@@ -1,6 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
+  serverDependenciesToBundle: "all",
   future: {
     v2_routeConvention: true,
     v2_meta: true,
